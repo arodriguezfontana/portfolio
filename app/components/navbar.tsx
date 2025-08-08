c3e7529dedf99a6bb05616121474d87e1e8cd93a
Abril Rodríguez <112506401+arodriguezfontana@users.noreply.github.com>
2025-08-08 14:17:40 -0300

    wip navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -18,8 +18,9 @@ const Navbar = () => {
         </ul>
 
         <div>
-            <h3>...</h3>
-            {/* <a href="#contact">Contact <Image src={assets.arrow_icon} alt='arrow' className='w-3'/></a> */}
+            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contacto
+                 {/* <Image src={assets.arrow_icon} alt='arrow' className='w-3'/> */}
+            </a>
         </div>
       </nav>
     </>
