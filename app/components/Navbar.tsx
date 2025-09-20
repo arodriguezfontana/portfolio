419f22a053fb2017c2a8d88fbf5133b9896e0bb6
Abril <arodriguezfontana@gmail.com>
2025-09-19 21:48:55 -0300

    wip navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <nav className='w-full fixed py-3 flex justify-center'>
+        <ul className='flex gap-3 rounded-full px-6 py-2 bg-white/40 backdrop-blur-md shadow-md text-sm'>
+          <li><a href="#experience">Experiencia</a></li>
+          <li><a href="#projects">Proyectos</a></li>
+          <li><a href="#about">Sobre mí</a></li>
+          <li><a href="#contact">Contacto</a></li>
+        </ul>
+      </nav>
+    </>
   )
 }
 
